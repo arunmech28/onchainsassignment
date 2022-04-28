df3f9d5b6a2a1803efc46a9ba24c530056cee35d
arunmech28 <38682818+arunmech28@users.noreply.github.com>
2022-04-28 12:02:37 +0530

    Un commented GitHub code

--- src/components/server/ChatContent.js
+++ src/components/server/ChatContent.js
@@ -5,21 +5,21 @@ import Pusher from "pusher-js";
 
 const ChatContent = () => {
     const [messages, setMessages] = useState([]);
-    // const pusher = new Pusher("61a2b9cf3efcc637be74", {
-    //     cluster: "ap2",
-    // });
+    const pusher = new Pusher("61a2b9cf3efcc637be74", {
+      cluster: "ap2",
+     });
 
-    // useEffect(() => {
-    //     const channel = pusher.subscribe("onchains-assignment-arun");
-    //     channel.bind("messages", (data) => {
-    //         const { username, message } = data;
-    //         setMessages((prevState) => [...prevState, { username, message }]);
-    //     });
+    useEffect(() => {
+         const channel = pusher.subscribe("onchains-assignment-arun");
+         channel.bind("messages", (data) => {
+             const { username, message } = data;
+             setMessages((prevState) => [...prevState, { username, message }]);
+         });
 
-    //     return () => {
-    //         pusher.unsubscribe("onchains-assignment-arun");
-    //     };
-    // }, []);
+         return () => {
+            pusher.unsubscribe("onchains-assignment-arun");
+        };
+    }, []);
 
     return (
         <div className="chat-content">
